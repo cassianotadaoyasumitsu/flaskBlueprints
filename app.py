b01cc74e16bc27e6f243691399b9a5304240588d
cassiano yasumitsu <cassiano.yasumitsu@wealth-park.com>
2023-02-20 21:32:34 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from home import home_bp
+from contact import contact_bp
+
+app = Flask(__name__)
+
+
+app.register_blueprint(home_bp, url_prefix='/home')
+app.register_blueprint(contact_bp, url_prefix='/contact')
+
+
+if __name__ == '__main__':
+    app.run()
